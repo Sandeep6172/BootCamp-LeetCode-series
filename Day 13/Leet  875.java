ae9c182d8711f9c3b8b20ba71321cc4b5663f4f1
Sandeep Kumar <sandeep.kumar.cs27@iilm.edu>
2026-09-02 13:41:34 +0530

    Add binary search for minimum eating speed calculation
    Implement binary search to find the minimum eating speed.

---
+++ Day 13/Leet  875.java
@@ -0,0 +1,24 @@
+int end = 0;
+        int start = 1;
+        int result = 0;
+        for (int pile : piles) {
+            end = Math.max(end, pile);
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long total = 0;
+            for (int pile : piles) {
+                total += pile / mid;
+                if (pile % mid != 0) {
+                    total++;
+                }
+            }
+            if (total <= h) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
